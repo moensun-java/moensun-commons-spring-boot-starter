63fa4e6314d4ed94906675618db322230ad58b7f
baneshi <fengxiaotx@163.com>
2018-02-01 00:20:32 +0800

    [Bane.Shi]

---
+++ src/main/java/com/moensun/commons/autoconfigure/AutoConfiguration.java
@@ -0,0 +1,39 @@
+package com.moensun.commons.autoconfigure;
+
+import com.moensun.commons.spring.resource.MSResourceBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Bane.Shi.
+ * Copyright MoenSun
+ * User: Bane.Shi
+ * Date: 2017/7/30
+ * Time: 13:10
+ */
+@Configuration
+@EnableConfigurationProperties(
+        value = {
+                CommonsProperties.class
+        }
+)
+@ComponentScan(
+        basePackages = {"com.moensun.commons.spring.context"}
+)
+public class AutoConfiguration {
+    @Autowired
+    private CommonsProperties commonsProperties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MSResourceBundle msResourceBundle(){
+        MSResourceBundle  msResourceBundle = new MSResourceBundle();
+        msResourceBundle.setResourceBundle(commonsProperties.getResourcePath());
+        return msResourceBundle;
+    }
+
+}
---
+++ src/main/java/com/moensun/commons/autoconfigure/CommonsProperties.java
@@ -0,0 +1,25 @@
+package com.moensun.commons.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by Bane.Shi.
+ * Copyright MoenSun
+ * User: Bane.Shi
+ * Date: 2017/7/30
+ * Time: 13:18
+ */
+@ConfigurationProperties(
+        prefix = "moensun.commons"
+)
+public class CommonsProperties {
+    private String resourcePath;
+
+    public String getResourcePath() {
+        return resourcePath;
+    }
+
+    public void setResourcePath(String resourcePath) {
+        this.resourcePath = resourcePath;
+    }
+}
