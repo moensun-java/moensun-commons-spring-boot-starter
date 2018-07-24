6a2c45691d3a0da63d578b0f9ca831669e160430
fengxiaotx <fengxiaotx@163.com>
2018-07-24 14:43:24 +0800

    [Bane.Shi]

--- src/main/java/com/moensun/commons/autoconfigure/CommonsProperties.java
+++ src/main/java/com/moensun/commons/autoconfigure/CommonsProperties.java
@@ -15,11 +15,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class CommonsProperties {
     private String resourcePath;
 
+    private String[] resourcePaths;
+
     public String getResourcePath() {
         return resourcePath;
     }
 
     public void setResourcePath(String resourcePath) {
         this.resourcePath = resourcePath;
+    }
+
+    public String[] getResourcePaths() {
+        return resourcePaths;
+    }
+
+    public void setResourcePaths(String[] resourcePaths) {
+        this.resourcePaths = resourcePaths;
     }
 }
